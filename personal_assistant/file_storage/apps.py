ba8c883fc35ace4170ed099e24fa001e07c72966
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-13 19:59:35 +0100

    functions for aws storage

--- personal_assistant/file_storage/apps.py
+++ personal_assistant/file_storage/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class FileStorageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'file_storage'
+
+    def ready(self):
+        from . import signals
+
