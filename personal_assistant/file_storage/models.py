1061a1a4d5e5b73f8886e481cbf6c74fd0dfa7cd
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-13 20:00:42 +0100

    upd models for file storage

--- personal_assistant/file_storage/models.py
+++ personal_assistant/file_storage/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.conf import settings
+
+
+def user_directory_path(instance, filename):
+    extension = filename.split('.')[-1]
+
+    # return f'files/test/{extension}/{filename}'
+    return f'files/{instance.user.id}/{extension}/{filename}'
 
 
 class File(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
-    file_path = models.CharField(max_length=255, blank=False, null=False)
+    file = models.FileField(upload_to=user_directory_path, null=True)
 
     def __str__(self):
         return f"{self.title}"
-
-
