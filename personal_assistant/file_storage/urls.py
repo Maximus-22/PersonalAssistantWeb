996232e51502d30c6747edbdb5ffb678267454c6
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-14 00:25:19 +0100

    add url for delete

--- personal_assistant/file_storage/urls.py
+++ personal_assistant/file_storage/urls.py
@@ -6,4 +6,5 @@ app_name = 'file_storage'
 urlpatterns = [
     path('', views.all_files, name='all_files'),
     path('upload/', views.file_upload_view, name='file-upload'),
+    path('delete/<int:file_id>/', views.delete_file, name='delete-file'),
 ]
