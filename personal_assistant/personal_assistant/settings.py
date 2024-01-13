a139b6b551a5893bfc5e35bee1dbc91a08c69d3e
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-13 21:23:07 +0100

    upd settings aws region

--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -130,7 +130,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = 'Europe (Paris) eu-west-3'
+AWS_S3_REGION_NAME = 'eu-west-3'
 
 # Using S3 for file storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
