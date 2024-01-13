a2fca5a14ab0f8db327c2bc9eb30ec3e0b38ef5e
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-13 21:22:19 +0100

    upd models

--- personal_assistant/file_storage/models.py
+++ personal_assistant/file_storage/models.py
@@ -6,11 +6,13 @@ def user_directory_path(instance, filename):
     extension = filename.split('.')[-1]
 
     # return f'files/test/{extension}/{filename}'
-    return f'files/{instance.user.id}/{extension}/{filename}'
+    # return f'files/{instance.user.id}/{extension}/{filename}'
+    user_folder = f'{instance.user.id}/' if instance.user else ''
+    return f'files/{user_folder}{extension}/{filename}'
 
 
 class File(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
     file = models.FileField(upload_to=user_directory_path, null=True)
