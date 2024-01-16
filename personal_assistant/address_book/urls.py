53e6cf3ac1ec0984932d9b6ccc076bb7a513ec49
TimurVerba <135266210+TimurVerba@users.noreply.github.com>
2024-01-16 12:07:34 +0100

    new_templates
    123

--- personal_assistant/address_book/urls.py
+++ personal_assistant/address_book/urls.py
@@ -1,8 +1,16 @@
+
+
 from django.urls import path
-
 from . import views
-
 app_name = 'address_book'
+
 urlpatterns = [
-    path('', views.all_contacts, name='all_contacts'),
+    path('all_contacts/', views.all_contacts, name='all_contacts'),
+    path('contact_list/', views.contact_list, name='contact_list'),
+    path('contact_detail/<int:pk>/', views.contact_detail, name='contact_detail'),
+    path('contact_add/', views.contact_add, name='contact_add'),
+    path('contact_edit/<int:pk>/', views.contact_edit, name='contact_edit'),
+    path('contact_delete/<int:pk>/', views.contact_delete, name='contact_delete'),
+    path('upcoming_birthdays/', views.upcoming_birthdays, name='upcoming_birthdays'),
+    path('contact_search/', views.contact_search, name='contact_search'),
 ]
--- personal_assistant/address_book/views.py
+++ personal_assistant/address_book/views.py
@@ -1,5 +1,77 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import AddressBook
+from .forms import AddressBookForm, SearchContactForm
+from django.utils import timezone
+from django.db.models import Q
 
 
 def all_contacts(request):
     return render(request, "address_book/all_contacts.html")
+
+
+def contact_list(request):
+    contacts = AddressBook.objects.all()
+    return render(request, 'address_book/contact_list.html', {'contacts': contacts})
+
+
+def contact_detail(request, pk):
+    contact = get_object_or_404(AddressBook, pk=pk)
+    return render(request, 'address_book/contact_detail.html', {'contact': contact})
+
+
+def contact_add(request):
+    if request.method == 'POST':
+        form = AddressBookForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = AddressBookForm()
+    return render(request, 'address_book/contact_edit.html', {'form': form})
+
+
+def contact_edit(request, pk):
+    contact = get_object_or_404(AddressBook, pk=pk)
+    if request.method == 'POST':
+        form = AddressBookForm(request.POST, instance=contact)
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = AddressBookForm(instance=contact)
+    return render(request, 'address_book/contact_edit.html', {'form': form})
+
+
+def contact_delete(request, pk):
+    contact = get_object_or_404(AddressBook, pk=pk)
+    contact.delete()
+    return redirect('contact_list')
+
+
+def upcoming_birthdays(request, days=7):
+    today = timezone.now()
+    upcoming_birthdays = AddressBook.objects.filter(birthday__range=[today, today + timezone.timedelta(days=days)])
+    return render(request, 'address_book/upcoming_birthdays.html', {'upcoming_birthdays': upcoming_birthdays})
+
+
+def contact_search(request):
+    if request.method == 'GET':
+        form = SearchContactForm(request.GET)
+        if form.is_valid():
+            search_term = form.cleaned_data['search_term']
+
+            contacts = AddressBook.objects.filter(
+                Q(first_name__icontains=search_term) |
+                Q(last_name__icontains=search_term) |
+                Q(address__icontains=search_term) |
+                Q(phone__icontains=search_term) |
+                Q(email__icontains=search_term)
+            )
+
+            return render(request, 'address_book/contact_search_results.html',
+                          {'contacts': contacts, 'search_term': search_term})
+    else:
+        form = SearchContactForm()
+
+    return render(request, 'address_book/contact_search.html', {'form': form})
