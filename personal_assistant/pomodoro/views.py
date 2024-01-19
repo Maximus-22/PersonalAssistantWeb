9dfa51d3ff5d7279522028027d1bb11e83f06e8b
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-19 09:38:45 +0100

    updated date format

--- personal_assistant/pomodoro/views.py
+++ personal_assistant/pomodoro/views.py
@@ -3,27 +3,28 @@ import json
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.utils.formats import date_format
+
 from .models import PomodoroSession
 from django.utils import timezone
 
 
 @login_required
 def start_pomodoro(request, session_type):
-    print("Request received:", request.method, request.headers.get('X-Requested-With'))
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest' and request.method == 'POST':
         data = json.loads(request.body)
         duration = data.get('duration')
-        print("Duration received:", duration)
         new_session = PomodoroSession.objects.create(
             user=request.user,
             start_time=timezone.now(),
             duration=duration,
             session_type=session_type
         )
+        formatted_start_time = date_format(new_session.start_time, "d F Y H:i")
         return JsonResponse({
             "success": True,
             "session_type": new_session.get_session_type_display(),
-            "start_time": new_session.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "start_time": formatted_start_time,
             "duration": new_session.duration
         })
     return JsonResponse({"success": False}, status=400)
